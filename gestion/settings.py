46ded1fd370f2436dd5c9ae13209987db70e4dbc
Antony Botello <antonybotello@gmail.com>
2022-04-22 07:41:27 -0500

    login

--- gestion/settings.py
+++ gestion/settings.py
@@ -135,3 +135,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# gestion de logueo
+LOGOUT_REDIRECT_URL = 'inicio'
+LOGIN_REDIRECT_URL = 'inicio'
+LOGIN_URL="usuario-login"
--- personal/urls.py
+++ personal/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from personal.views import aprendiz, equipo
 
 urlpatterns = [
     path('aprendiz/', aprendiz, name='personal-aprendiz'),
     path('equipo/', equipo, name='personal-equipo'),
+    
+    # Logueo
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='usuario-login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='usuario-logout'),
     
 ]
