92f588889f2e5ed203c20bb2a32d3574a5985a0b
Antony Botello <antonybotello@gmail.com>
2022-06-06 13:35:19 -0500

    backup modify

--- gestion/models.py
+++ gestion/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.zip']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Archivo no válido')
+
 class Backup(models.Model):
     nombre = models.CharField(max_length = 200,default="Copia de Seguridad", blank=True)
-    archivo = models.FileField(upload_to = "backup")
+    archivo = models.FileField(upload_to = "backup",validators=[validate_file_extension])
     fecha = models.DateTimeField(auto_now = True)
--- gestion/views.py
+++ gestion/views.py
@@ -5,8 +5,10 @@ import sys
 from django.core import management
 from gestion.models import Backup
 from gestion.forms import BackupForm
-
-
+from django.http import FileResponse
+import os
+import zipfile
+from datetime import date
 def crear_archivos(app_name):
     myfile = Path(f"static/backup/{app_name}.json")
     myfile.touch(exist_ok=True)
@@ -17,12 +19,22 @@ def extraer_datos(app_name):
         management.call_command('dumpdata', app_name)
         sys.stdout = sysout
 
-def rstaurar_datos(app_name):
-    for i in range(2):
-        sysout = sys.stdout
-        sys.stdout=open(f"static/backup/{app_name}.json", 'w+')
-        management.call_command('dumpdata', app_name)
-        sys.stdout = sysout
+def restaurar_datos(ruta_zip):
+
+
+    ruta_extraccion = ""
+    password = None
+    print(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {ruta_zip}")
+    archivo_zip = zipfile.ZipFile(ruta_zip[1:], "r")
+    try:
+        print(archivo_zip.namelist())
+        archivo_zip.extractall(pwd=password, path=ruta_extraccion)
+    except:
+        print("paila")
+    archivo_zip.close()
+    django.core.management.call_command("loaddata", ruta , ignorenonexistent=ignorenonexistent)
+    
+
         
 def subir_datos(app_name,f):
     with open(f"static/backup/{app_name}.json", 'wb+') as destination:
@@ -50,33 +62,48 @@ def backup(request,tipo):
     backups = Backup.objects.all()
     if request.method == 'POST' and tipo== "U":
         # Fetching the form data
-        nombre = "prueba"
+        
         form = BackupForm(request.POST, request.FILES)
         if form.is_valid():
-  
+            nombre= request.POST['nombre']
             archivo = request.FILES['archivo']
             
-            insert = Backup(nombre="copia", archivo=archivo)
+            insert = Backup(nombre=nombre, archivo=archivo)
             insert.save()
-            
+            restaurar_datos(insert.archivo.url)
             return redirect('backup','A')
         else:
             print( "Error al procesar el formulario")
-            return redirect('backup','A')
-
-        
-        return redirect('backup','A')
-            
-    elif request.method == 'POST'and tipo== "D":
+              
+    elif request.method == 'POST' and tipo== "D":
+        fecha= date.today()
         for modelo in modelos:
             if modelo.verbose_name in request.POST.getlist("modelos_lista"):
                 filtrado.append(modelo)
         print(filtrado)
-        return redirect('backup','A')
-    for i in filtrado:
-        crear_archivos(i)
-        extraer_datos(i)
         
+        for i in filtrado:
+            crear_archivos(i)
+            extraer_datos(i)
+        try:    
+            compression = zipfile.ZIP_DEFLATED
+        except:
+            compression = zipfile.ZIP_STORED
+        
+        zf = zipfile.ZipFile(f"static/backup/backup at {fecha}.zip", mode="w")
+        try:
+            for i in filtrado:
+                path_to_apk=f"static/backup/action/{i}.json"
+                zf.write(path_to_apk, compress_type=compression)    
+        finally:
+            zf.close()
+        path_to_apk=f"static/backup/backup at {fecha}.zip"
+        file=open(path_to_apk,"rb")
+        response=FileResponse(file)
+        response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(path_to_apk) 
+        
+        return response
+    
     else:
         form = BackupForm()
       
